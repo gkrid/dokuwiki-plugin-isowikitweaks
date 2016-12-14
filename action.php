b1cb26a06e869162b8a658bda675bbe327027f24
Szymon Olewniczak <(my first name) [at] imz [dot] re>
2016-12-14 17:53:21 +0100

    turnoff https redirect

--- action.php
+++ action.php
@@ -11,23 +11,4 @@ require_once(DOKU_PLUGIN . 'action.php');
 
 class action_plugin_isowikitweaks extends DokuWiki_Action_Plugin {
 
-    /**
-     * Register the events
-     */
-    function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'redirect');
-    }
-    
-    function redirect() {
-		if ($_SERVER["HTTPS"] == NULL) {
-			header("HTTP/1.1 301 Moved Permanently");
-			if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
-				$location  = 'https://' . substr($_SERVER['HTTP_HOST'], 4).$_SERVER['REQUEST_URI'];
-			} else {
-				$location = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-			}
-			header("Location: $location");
-			exit;
-		}
-	}
 }
